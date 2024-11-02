70c70c3234d2987b744a155f10ddd764d4e1a681
Siddesh Nageswaran <sidnages@gmail.com>
2024-11-02 11:26:03 -0400

    Create basic responsive calendar skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,101 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="calendar">
+        <div className="test">
+          <p>1</p>
+        </div>
+        <div className="test">
+          <p>2</p>
+        </div>
+        <div className="test">
+          <p>3</p>
+        </div>
+        <div className="test">
+          <p>4</p>
+        </div>
+        <div className="test">
+          <p>5</p>
+        </div>
+        <div className="test">
+          <p>6</p>
+        </div>
+        <div className="test">
+          <p>7</p>
+        </div>
+        <div className="test">
+          <p>8</p>
+        </div>
+        <div className="test">
+          <p>9</p>
+        </div>
+        <div className="test">
+          <p>10</p>
+        </div>
+        <div className="test">
+          <p>11</p>
+        </div>
+        <div className="test">
+          <p>12</p>
+        </div>
+        <div className="test">
+          <p>13</p>
+        </div>
+        <div className="test">
+          <p>14</p>
+        </div>
+        <div className="test">
+          <p>15</p>
+        </div>
+        <div className="test">
+          <p>16</p>
+        </div>
+        <div className="test">
+          <p>17</p>
+        </div>
+        <div className="test">
+          <p>18</p>
+        </div>
+        <div className="test">
+          <p>19</p>
+        </div>
+        <div className="test">
+          <p>20</p>
+        </div>
+        <div className="test">
+          <p>21</p>
+        </div>
+        <div className="test">
+          <p>22</p>
+        </div>
+        <div className="test">
+          <p>23</p>
+        </div>
+        <div className="test">
+          <p>24</p>
+        </div>
+        <div className="test">
+          <p>25</p>
+        </div>
+        <div className="test">
+          <p>26</p>
+        </div>
+        <div className="test">
+          <p>27</p>
+        </div>
+        <div className="test">
+          <p>28</p>
+        </div>
+        <div className="test">
+          <p>29</p>
+        </div>
+        <div className="test">
+          <p>30</p>
+        </div>
+        <div className="test">
+          <p>31</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
